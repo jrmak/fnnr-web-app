548071f22ff338916d435560aa597f6b42a9a609
jrmak <jmak@sdsu.edu>
2019-01-02 22:13:11 -0800

    Update ModularVisualization.py

--- ModularVisualization.py
+++ ModularVisualization.py
@@ -328,7 +328,8 @@ class ModularServer(tornado.web.Application):
         if port is not None:
             self.port = port
         #ip = os.environ.get('OPENSHIFT_PYTHON_IP', 'localhost')
-        ip = os.environ.get('PORT', 5000)
+        # ip = os.environ.get('PORT', 5000)
+        ip ='0.0.0.0'
         url = 'http://{IP}:{PORT}'.format(IP=ip, PORT=self.port)
         print('Interface starting at {url}'.format(url=url))
         self.listen(self.port, ip)
