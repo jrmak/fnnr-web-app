da010aaad803370d0ba0772acd7be30ebe3fd288
jrmak <jmak@sdsu.edu>
2019-01-02 22:06:31 -0800

    Update ModularVisualization.py

--- ModularVisualization.py
+++ ModularVisualization.py
@@ -327,7 +327,8 @@ class ModularServer(tornado.web.Application):
         startLoop = not tornado.ioloop.IOLoop.initialized()
         if port is not None:
             self.port = port
-        ip = os.environ.get('OPENSHIFT_PYTHON_IP', 'localhost')
+        #ip = os.environ.get('OPENSHIFT_PYTHON_IP', 'localhost')
+        ip = os.environ.get('PORT', 5000)
         url = 'http://{IP}:{PORT}'.format(IP=ip, PORT=self.port)
         print('Interface starting at {url}'.format(url=url))
         self.listen(self.port, ip)
